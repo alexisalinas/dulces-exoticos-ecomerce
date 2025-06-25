dc9b52166fba77c00e34044a9c092333b5a9d379
alexis soto <alexisalinasoto147@gmail.com>
2025-06-25 14:33:28 -0600

    Corregido nombre de tabla 'user' para evitar conflictos SQL

--- src/main/java/login/login/model/User.java
+++ src/main/java/login/login/model/User.java
@@ -1,9 +1,12 @@
 package login.login.model;
 
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "users")
 public class User {
     @Id
     private String username;
